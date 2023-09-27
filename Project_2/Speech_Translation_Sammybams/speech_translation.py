5f3f5212ad2252313c997aaa099705fbc047556e
Sammybams <samuelbamgbola@gmail.com>
2023-09-27 11:44:53 +0100

    Edited my submission.

---
+++ Project_2/Speech_Translation_Sammybams/speech_translation.py
@@ -0,0 +1,36 @@
+import requests, uuid, json
+
+# Add your key and endpoint
+key = "<your-translator-key>"
+endpoint = "https://api.cognitive.microsofttranslator.com"
+
+# location, also known as region.
+# required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+location = "<YOUR-RESOURCE-LOCATION>"
+
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['fr', 'zu']
+}
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key,
+    # location required if you're using a multi-service or regional (not global) resource.
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+# You can pass more than one object in body.
+body = [{
+    'text': 'I would really like to drive your car around the block a few times!'
+}]
+
+request = requests.post(constructed_url, params=params, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
