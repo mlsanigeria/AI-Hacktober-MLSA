d36b309c9ef58f5171b80a14615f66bfba549e8e
Samuel Bamgbola <samuelbamgbola@gmail.com>
2023-09-28 11:02:48 +0100

    Updated success message to refer to LEADERBOARD.md

--- update_leaderboard.py
+++ update_leaderboard.py
@@ -85,6 +85,6 @@ markdown_content = """
 # Write the Markdown content to README.md
 with open("LEADERBOARD.md", "w") as readme_file:
     readme_file.write(markdown_content)
-    print("successfully updated README.md")
+    print("successfully updated LEADERBOARD.md")
 
 
