501ba439962285ce498da5f07a3c1e13ebf0af8f
Sammybams <samuelbamgbola@gmail.com>
2023-09-30 21:42:08 +0100

    Updated directory path.

--- resize_image_data.py
+++ resize_image_data.py
@@ -3,7 +3,7 @@ import os
 
 building_types = ["Bungalow", "High-rise", "Storey-building"]
 for building in building_types:
-    directory_path = f"/AI-Hacktober-MLSA/Project_1/Data/{building}"
+    directory_path = f"/home/runner/work/AI-Hacktober-MLSA/AI-Hacktober-MLSA/Project_1/Data/{building}"
     filenames = os.listdir(directory_path)
     index = 1
     for filename in filenames:
