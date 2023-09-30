2d3521527a4cf62e100deb23bfa0cb779dc6af49
Sammybams <samuelbamgbola@gmail.com>
2023-09-30 21:59:56 +0100

    Updated script to resize images.

--- resize_image_data.py
+++ resize_image_data.py
@@ -2,28 +2,35 @@ from PIL import Image
 import os
 
 building_types = ["Bungalow", "High-rise", "Storey-building"]
-for building in building_types:
-    directory_path = f"/home/runner/work/AI-Hacktober-MLSA/AI-Hacktober-MLSA/Project_1/Data/{building}"
-    filenames = os.listdir(directory_path)
-    index = 1
-    for filename in filenames:
-        try:
-            image = Image.open(f"{directory_path}/{filename}")
-        except:
-            continue
 
-        if image.size==(300,400):
-            os.remove(f"{directory_path}/{filename}")
-            if filename.split(".")[-1]!="jpeg":
-                filename = "".join([filename.split(".")[0], ".jpeg"])
-            image.save(f"{directory_path}/{filename}")
-            index+=1
-            
-        else:
-            new_image = image.resize((300, 400))
-            if filename.split(".")[-1]!="jpeg":
-                filename = "".join([filename.split(".")[0], ".jpeg"])
-            new_image.save(f"{directory_path}/{filename}")
-            index+=1
-            
+# Define the path to the data directory within your GitHub repository
+repository_path = "/github/workspace/AI-Hacktober-MLSA/AI-Hacktober-MLSA/Project_1/Data"
+
+for building in building_types:
+    directory_path = os.path.join(repository_path, building)
+    
+    # Check if the directory exists
+    if os.path.exists(directory_path):
+        filenames = os.listdir(directory_path)
+        index = 1
+        for filename in filenames:
+            try:
+                image = Image.open(os.path.join(directory_path, filename))
+            except Exception as e:
+                print(f"Error opening {filename}: {str(e)}")
+                continue
+
+            if image.size == (300, 400):
+                os.remove(os.path.join(directory_path, filename))
+                if filename.split(".")[-1] != "jpeg":
+                    filename = "".join([filename.split(".")[0], ".jpeg"])
+                image.save(os.path.join(directory_path, filename))
+                index += 1
+            else:
+                new_image = image.resize((300, 400))
+                if filename.split(".")[-1] != "jpeg":
+                    filename = "".join([filename.split(".")[0], ".jpeg"])
+                new_image.save(os.path.join(directory_path, filename))
+                index += 1
+
 print("Successfully resized images for Project 1.")
