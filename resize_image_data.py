220a410a872971c2874f0a340df5c6f2775f40dc
Sammybams <samuelbamgbola@gmail.com>
2023-10-04 13:59:22 +0100

    Commented out print statements used to debug code.

--- resize_image_data.py
+++ resize_image_data.py
@@ -9,17 +9,17 @@ repository_path = "./Project_1/Data/"
 
 for building in building_types:
     directory_path = os.path.join(repository_path, building)
-    print(directory_path)
+    # print(directory_path)
     # Check if the directory exists
     if os.path.exists(directory_path):
         filenames = os.listdir(directory_path)
         index = 1
         for filename in filenames:
-            print(filename)
+            # print(filename)
             try:
                 image = Image.open(os.path.join(directory_path, filename))
             except Exception as e:
-                print(f"Error opening {filename}: {str(e)}")
+                # print(f"Error opening {filename}: {str(e)}")
                 continue
 
             if image.size == (300, 400):
