dd5169182e4b2706854284e3e142135b432f8e93
Samuel Bamgbola <samuelbamgbola@gmail.com>
2023-10-20 17:05:37 +0000

    Added input for maths and english grades.

--- Project_3/app.py
+++ Project_3/app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-import streamlit as st
-
 jamb_score = st.number_input('What was your Jamb score')
+# st.write('Your jamb score was ', jamb_score)
+
+maths = st.selectbox(
+    'What was your WAEC grade in Maths?',
+    ('A', 'B', 'C', 'D', 'E', 'F'))
+
+english = st.selectbox(
+    'What was your WAEC grade in English?',
+    ('A', 'B', 'C', 'D', 'E', 'F'))
+
 st.write('Your jamb score was ', jamb_score)
+st.write('Your Maths WAEC grade was ', maths)
+st.write('Your English WAEC grade was ', english)
