cbf8bf37f72eac8cb326d01e0f454961a35dd00a
Samuel Bamgbola <samuelbamgbola@gmail.com>
2023-11-01 12:38:58 +0000

    Added new features to web interface script and demo audio.

--- Project_2/web.py
+++ Project_2/web.py
@@ -2,6 +2,10 @@ import streamlit as st
 from SpeechTranslation import translate
 from speech import Transcribe
 
+# st.header("Speech Translation")
+st.markdown("# Speech Translation")
+st.markdown("### (From English to Yoruba, Igbo or Hausa)")
+
 option = st.selectbox(
     'What would you like to translate?',
     ('A text', 'An audio file'))
@@ -15,10 +19,16 @@ dict_lang = {"Yoruba": "yo",
              "Hausa": "ha"}
 
 if option=='A text':
-    title = st.text_input('English Text', 'Enter your english text here?')
+    title = st.text_area('English Text', 'Enter your english text here?')
     if st.button('Translate'):
-        translation = translate(dict_lang[language], title)
-        st.write(f'Your translation from English to {language} is: ', translation)
+        with st.spinner(f"Translating {option.split()[1].capitalize()}..."):
+            translation = translate(dict_lang[language], title)
+
+        st.write(f'Your translation from English to {language} is: ')
+        # st.markdown(f"#### {translation}")
+        # st.code(f"{translation}", language="markdown")
+        # st.text(translation)
+        title = st.text_area('', translation)
 
 elif option== 'An audio file':
     uploaded_file = st.file_uploader("Upload an english audio file")
